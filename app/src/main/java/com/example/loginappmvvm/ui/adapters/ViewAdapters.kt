c0f1bb382a5f084ab3c369998250559a4726a737
Rahul Gupta <gupta8914@gmail.com>
2020-09-28 23:58:10 +0530

    BindingAdapter

--- app/src/main/java/com/example/loginappmvvm/ui/adapters/ViewAdapters.kt
+++ app/src/main/java/com/example/loginappmvvm/ui/adapters/ViewAdapters.kt
@@ -1,2 +1,27 @@
 package com.example.loginappmvvm.ui.adapters
 
+import android.view.View
+import android.widget.Button
+import androidx.databinding.BindingAdapter
+import com.google.android.material.textfield.TextInputLayout
+
+
+@BindingAdapter("buttonTextLabel")
+fun bindButtonTextLabel(button: Button, buttonTextLabel: String){
+    button.text = buttonTextLabel.capitalize()
+}
+
+@BindingAdapter("hintText")
+fun bindHintText(textInputLayout: TextInputLayout, hintText: String){
+    textInputLayout.hint = hintText.capitalize()
+}
+
+@BindingAdapter("visibleOrGone")
+fun View.setVisibleOrGone(show: Boolean) {
+    visibility = if (show) View.VISIBLE else View.GONE
+}
+
+@BindingAdapter("visible")
+fun View.setVisible(show: Boolean) {
+    visibility = if (show) View.VISIBLE else View.INVISIBLE
+}
